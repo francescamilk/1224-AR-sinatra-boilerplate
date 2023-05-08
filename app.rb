a260f54cfbc42e68366b11afa26ae055b34d940b
Francesca Santoriello <f.santoriello@hotmail.com>
2023-05-08 15:18:34 +0200

    sp

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
 
+## Acts as controller and router together
+
+# Action
 get "/" do
-  "Hello world!"
+    # Homepage
+    erb :index
 end
